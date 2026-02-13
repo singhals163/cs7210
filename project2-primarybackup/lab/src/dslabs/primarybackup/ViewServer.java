9d84524a78fa0c87a58a9cda509aa2077b7f1f12
Shivam Singhal <shivamsinghal16032003@gmail.com>
2026-02-13 14:56:19 -0500

    Added view server implementation

--- project2-primarybackup/lab/src/dslabs/primarybackup/ViewServer.java
+++ project2-primarybackup/lab/src/dslabs/primarybackup/ViewServer.java
@@ -14,11 +14,12 @@ import java.util.HashMap;
 class ViewServer extends Node {
   static final int STARTUP_VIEWNUM = 0;
   private static final int INITIAL_VIEWNUM = 1;
+  private static final int PING_MISSES_AVAILABLE = 3;
 
   // Your code here...
-  private int primaryPingMiss, secondaryPingMiss;
+  private Map<Address, Integer> servers = new HashMap<>();
   private View currentView;
-  private Map<Address, int> servers = new HashMap<>();
+  private boolean primaryAck;
 
   /* -----------------------------------------------------------------------------------------------
    *  Construction and Initialization
@@ -31,9 +32,8 @@ class ViewServer extends Node {
   public void init() {
     set(new PingCheckTimer(), PING_CHECK_MILLIS);
     // Your code here...
-    primary_ping_miss = 0;
-    secondary_ping_miss = 0;
-
+    currentView = new View(STARTUP_VIEWNUM, null, null);
+    primaryAck = false;
   }
 
   /* -----------------------------------------------------------------------------------------------
@@ -41,13 +41,19 @@ class ViewServer extends Node {
    * ---------------------------------------------------------------------------------------------*/
   private void handlePing(Ping m, Address sender) {
     // Your code here...
-    // TODO: Figure out what to do with the m.num
-    servers.put(sender, 2);
-
+    if(m.viewNum() == currentView.viewNum() && sender == currentView.primary()) {
+      primaryAck = true;
+    }
+    servers.put(sender, PING_MISSES_AVAILABLE);
+    if(currentView.viewNum() == STARTUP_VIEWNUM) {
+      generateNewView();
+    }
+    send(new ViewReply(currentView), sender);
   }
-
+  
   private void handleGetView(GetView m, Address sender) {
     // Your code here...
+    send(new ViewReply(currentView), sender);
   }
 
   /* -----------------------------------------------------------------------------------------------
@@ -55,16 +61,57 @@ class ViewServer extends Node {
    * ---------------------------------------------------------------------------------------------*/
   private void onPingCheckTimer(PingCheckTimer t) {
     // Your code here...
-    if(primary_ping_miss > 2) {
-      // do something
-    } else if(secondary_ping_miss > 2) {
-      // do something
-    }
     set(t, PING_CHECK_MILLIS);
+    servers.replaceAll((key, value) -> value - 1);
+    servers.entrySet().removeIf(entry -> entry.getValue() == 0);
+    if(!servers.containsKey(currentView.primary()) || !servers.containsKey(currentView.backup())) {
+      generateNewView();
+    }
   }
 
   /* -----------------------------------------------------------------------------------------------
    *  Utils
    * ---------------------------------------------------------------------------------------------*/
   // Your code here...
+  private void generateNewView(){
+    Address primary = null, backup = null;
+    if(currentView.viewNum() == STARTUP_VIEWNUM) {
+      // create the first view
+      for(Map.Entry<Address, Integer> entry: servers.entrySet()) {
+        if(primary == null) {
+          primary = entry.getKey();
+        } else {
+          backup = entry.getKey();
+          break;
+        }
+      }
+      if(primary != null) {
+        currentView = new View(INITIAL_VIEWNUM, primary, backup);
+      }
+      return;
+    } else if(primaryAck) {
+      if(!servers.containsKey(currentView.primary()) && !servers.containsKey(currentView.backup())) {
+        // Both primary and server shouldn't have died, should return an exception here
+        return;
+      }
+      primaryAck = false; 
+      primary = currentView.primary();
+      if(!servers.containsKey(currentView.primary())) {
+        // promote backup as primary
+        primary = currentView.backup();
+      } 
+      for(Map.Entry<Address, Integer> entry: servers.entrySet()) {
+        if(entry.getKey() != primary) {
+          backup = entry.getKey();
+          break;
+        }
+      }
+      currentView = new View(currentView.viewNum() + 1, primary, backup);
+    }
+    // else {
+      // Ack not received from primary 
+      // or 
+      // primary died, in which no progress in the states
+    // }
+  }
 }
